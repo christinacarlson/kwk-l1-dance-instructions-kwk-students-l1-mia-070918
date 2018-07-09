f4b06c88039b82727abc75d58c4e8306208d257f
christinacarlson <christina.carlson213@gmail.com>
2018-07-09 19:51:36 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,6 +4,7 @@ puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
 puts "1. Lift right foot
       2. Return right foot to the ground
       3. Finishing with a **small skip-step** backward"
+puts 
 def gangnam style
   
 end
