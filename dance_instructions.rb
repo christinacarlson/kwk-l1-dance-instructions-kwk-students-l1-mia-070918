9567a819ecc5e9ce1a1c8d63e88b1a5e6a48a657
christinacarlson <christina.carlson213@gmail.com>
2018-07-09 20:21:16 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,7 +2,7 @@ def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end 
 
-def skip_step
+def skip_step(left)
   puts "skip a step (lower, bounce, briefly)"
 end
 
